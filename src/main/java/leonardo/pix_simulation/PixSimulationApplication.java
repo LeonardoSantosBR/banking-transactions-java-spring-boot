89cd9834ee9b489daf522805d71671e0ff1a857b
Leonardo Santos <leosantosmelo07@gmail.com>
2026-09-14 19:31:31 -0300

    init: Initialize project with Maven wrapper, Spring Boot setup, and basic application structure

---
+++ src/main/java/leonardo/pix_simulation/PixSimulationApplication.java
@@ -0,0 +1,13 @@
+package leonardo.pix_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leonardo/pix_simulation/PixSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardo.pix_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
